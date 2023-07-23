9cb273231f013574e2a58ce80e23f995ba8d8d0e
Jarugula <vamsi.jarugula@adp.com>
2023-07-24 01:16:01 +0530

    setup basic dotenv

---
+++ src/config/serverConfig.js
@@ -0,0 +1,8 @@
+const donenv = require('dotenv')
+
+
+donenv.config();
+
+module.export={
+    PORT:process.env.PORT 
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-//checking 
+const express=require('express')
+const {PORT} = require('./config/serverConfig')
+
+const startAndSetupServer= async()=> {
+    const app=express();
+    
+    app.listen(PORT,()=>{
+        console.log(`sever started ${PORT}`)
+    });
+}
+startAndSetupServer();
